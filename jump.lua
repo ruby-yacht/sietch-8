847274911630543e5a30b5fc7a5a29789d7afbab
rubaiyat <gitub@factorynoir.com>
2024-07-22 01:54:01 -0700

    sprites from photoshop

--- jump.lua
+++ jump.lua
@@ -63,7 +63,8 @@ function initPlayers()
     local keyInput
     playerCount = 0
     local posx = 8
-    local sprites = {1, 5, 6, 7, 8, 9, 10, 11} -- Example sprite indices
+    -- local sprites = {1, 5, 6, 7, 8, 9, 10, 11} 
+    local sprites = {33, 34, 35, 36, 37, 38, 10, 11} -- Example sprite indices
 
     repeat
         cls()
