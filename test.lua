242d520da0c11d656f4b66168b075ea0c814799e
Cole Pergerson <cole.pergerson@gmail.com>
2024-07-21 20:05:19 -0700

    Added comments to my code

--- test.lua
+++ test.lua
@@ -19,23 +19,20 @@ poke(0x5F2D, 0x1) -- enable keyboard input
 
 
 function _init()
-    cls(1)
-    
-    
-    print("Hop To Survive");
+    -- empty
 end
 
 function _update()
 
-    if gameStarted then
+    if gameStarted then -- main game loop
         local keyInput = ""
         updatePlayers()
 
-        if stat(30) then
+        if stat(30) then -- if key press was detected
             keyInput = stat(31)
             bouncePlayer(keyInput)
         end
-    else
+    else -- character select screen
         initPlayers()
 
         keyInput = ""
@@ -54,13 +51,14 @@ function drawPlayers()
     end
 end
 
-function updatePlayers()
+-- apply "physics" to all players
+function updatePlayers() 
     for key, player in pairs(players) do
         player.vy = player.vy + GRAVITY
 
         player.y = player.y + player.vy
 
-               -- Check collision with ground (assuming ground level is at y = 120)
+        -- Check collision with ground (assuming ground level is at y = 112)
         if player.y + player.height >= 112 then
             player.y = 112 - player.height
             player.vy = player.vy
@@ -71,46 +69,45 @@ function updatePlayers()
     end
 end
 
--- Function to handle players' bouncing
+-- look up key associated with player and bounce them
 function bouncePlayer(key)
-
     local player = players[key]
     if not (player == nil) and player.onGround then
         player.vy = BOUNCE_FACTOR
     end
 end
 
+-- spawn players
 function initPlayers()
-    print("Player count: " .. 11)
-    print("Player count: ", 20,20)
+    if stat(30) then -- was a key pressed?
+        local keyInput = stat(31) -- which key was pressed
 
-    if stat(30) then
-        local keyInput = stat(31)
+        -- if the key isn't the space button 
+        -- and if the key wasn't already pressed, add it to the player list
+        if not (keyInput == "\32") and not players[keyInput] then 
 
-        if not (keyInput == "\31") then
-    
-            if not players[keyInput] then
-                players[keyInput] = {x = posx, y = posy, width = 8, height = 8, vx = 0, vy = 0, onGround = false, key=keyInput}
+            -- create a player
+            players[keyInput] = {x = posx, y = posy, width = 8, height = 8, vx = 0, vy = 0, onGround = false, key=keyInput}
+            playerCount = playerCount + 1
 
-                playerCount = playerCount + 1
-                posx = posx + 9
-
-                if (posx >= 120) then
-                    
-                    if xOffset >= 8 then
-                        xOffset = 0
-                    else
-                        xOffset = xOffset + 2
-                    end
-
-                    posx = xOffset
-
-                    posy = posy + 9
+            -- edit the offset so not all players spawn in the same position
+            posx = posx + 9
+            if (posx >= 120) then
+                
+                if xOffset >= 8 then
+                    xOffset = 0
+                else
+                    xOffset = xOffset + 2
                 end
+
+                posx = xOffset
+
+                posy = posy + 9
             end
         end
 
-        if keyInput == "\32" then
+        -- exit player selection and start the game
+        if keyInput == "\32" then 
             gameStarted = true
         end
 
