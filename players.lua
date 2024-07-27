7d44599502a83a1b7bdd0c4bbc84b1574b170379
rubaiyat <gitub@factorynoir.com>
2024-07-26 21:44:15 -0700

    sprites, yo

--- players.lua
+++ players.lua
@@ -58,7 +58,7 @@ end
 
 -- spawn players
 function initPlayers()
-    local sprites = {33, 34, 35, 36, 37, 38, 10, 11}
+    local sprites = {33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64}
 
     if stat(30) then 
         local keyInput = stat(31) 
