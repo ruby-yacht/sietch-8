d3bde90f68449127e97d854f42874030ebc08226
Cole Pergerson <cole.pergerson@gmail.com>
2025-03-15 21:48:12 -0700

    Finished simple collision system. Corner edge case needs fix

--- proceduralgen.lua
+++ proceduralgen.lua
@@ -11,8 +11,8 @@ BIOME_DIST = {
     HELL = 96
 }
 
-chunk_x_size = BIOME_DIST.HELL
-chunk_y_size = 16
+map_x_size = BIOME_DIST.HELL
+map_y_size = 16
 camera_x = 0
 camera_y = 0
 -- tile ids: air = 0; grass = 2; ground = 3; wall = 4; 
@@ -38,40 +38,35 @@ TILE = {
     HELL_3 = 107
 }
 
-groundlevel = 12 -- relative to tiles, not pixels
+groundlevel = 11 -- relative to tiles, not pixels
 player = {} -- for testing
 
 function _init()
-
-    -- according to deepseek, this is "more memory-efficient and faster for large grids."
-    -- Please compare the performance when you get a chance
     -- Fill all cells with ground
-    for y = 1, chunk_y_size do
-        for x = 1, chunk_x_size do
-            local index = (y - 1) * chunk_x_size + x -- Calculate 1D index
-            
-
+    for x = 0, map_x_size-1 do
+        levelgen[x] = {}
+        for y = 0, map_y_size-1 do         
             if x <= BIOME_DIST.GRASS then -- MAKE THIS BETTER
-                levelgen[index] = {x = x, y = y, tile = TILE.GRASS}
+                levelgen[x][y] = {x = x, y = y, tile = TILE.GRASS}
             elseif x <= BIOME_DIST.DESERT then
-                levelgen[index] = {x = x, y = y, tile = TILE.SAND_1}
+                levelgen[x][y] = {x = x, y = y, tile = TILE.SAND_1}
             elseif x <= BIOME_DIST.MOUNTAIN then
-                levelgen[index] = {x = x, y = y, tile = TILE.MOUNTAIN_1}
+                levelgen[x][y] = {x = x, y = y, tile = TILE.MOUNTAIN_1}
             elseif x <= BIOME_DIST.SNOW then
-                levelgen[index] = {x = x, y = y, tile = TILE.SNOW_2}
+                levelgen[x][y] = {x = x, y = y, tile = TILE.SNOW_2}
             elseif x <= BIOME_DIST.ORELAND then
-                levelgen[index] = {x = x, y = y, tile = TILE.ORELAND_1}
+                levelgen[x][y] = {x = x, y = y, tile = TILE.ORELAND_1}
             elseif x <= BIOME_DIST.HELL then
-                levelgen[index] = {x = x, y = y, tile = TILE.HELL_2}
+                levelgen[x][y] = {x = x, y = y, tile = TILE.HELL_2}
             else
-                levelgen[index] = {x = x, y = y, tile = TILE.GROUND}
+                levelgen[x][y] = {x = x, y = y, tile = TILE.GROUND}
             end  
         end
     end
 
     -- generate ground by removing ground tiles.
-    for y = 1, chunk_y_size do
-        for x = 1, chunk_x_size do      
+    for x = 0, map_x_size-1 do
+        for y = 0, map_y_size-1 do      
             local h = get_cell_height_at_(x)  -- Normalize x to [0, 1] (remember to explain why dividing by chunk_x_size fixes sin output)
             --h = 2 * sin( ((x-1) / chunk_x_size) * 2)
             if y - groundlevel < h then
@@ -129,8 +124,86 @@ function biome_mountain_height_at_(x) -- raise ground level?
 end
 
 function _update()
+
+    hop_mode()
+    --test_mode()
+end
+
+function _draw()
+    cls()
+    camera(camera_x, camera_y)
+
+    local visibleTilesX = flr(camera_x) + 16
+    local visibleTilesY = flr(camera_y) + 16
+
+    for x = 0, map_x_size-1 do
+        for y = 0, map_y_size-1 do     
+            local tile = get_tile(x,y).tile
+            if tile > 0 then
+                
+                spr(tile, x * 8, y * 8)
+
+                --Debug
+                --print((y - 1) * 8, (x - 1) * 8, (y - 1) * 8)
+                --print(y, (x - 1) * 8, (y - 1) * 8)
+            end
+        end
+    end
+
+    --spr(99, new_pos_x, new_pos_y)
+    spr(player.sprite, player.x, player.y)
+
+end
+
+function get_tile(x, y)
+    if x < 0 or x >= map_x_size or y < 0 or y >= map_y_size then
+        printh("(" .. x .. "," .. y .. ") tile index is out of bounds")
+        return {tile = -1}
+    else
+        return levelgen[flr(x)][flr(y)]
+    end
+end
+
+function get_tile_at_pos(x, y)   
+    return get_tile(flr(x / 8) , flr(y / 8))
+end
+
+function test_mode()
+    camera_x = player.x - 56
+    --camera_y = player.y + 64
+
+    player.vx = 0
+    player.vy = 0
+
+    if btn(0) then
+        player.vx -= 2
+     end
+     
+     if btn(1) then
+        player.vx += 2
+     end
+     
+     if btn(2) then
+        player.vy -= 2
+     end
+     
+     if btn(3) then
+        player.vy += 2
+     end
+
+     local player_new_x = player.x + player.vx
+     local player_new_y = player.y + player.vy
+
+     checked_position = check_collision(player_new_x, player_new_y, player.x, player.y)
+
+     player.x = checked_position.x
+     player.y = checked_position.y
+    
+end
+
+function hop_mode()
     camera_x += .5
-    camera_x = min(camera_x, (chunk_x_size-16) * 8)
+    camera_x = min(camera_x, (map_x_size-16) * 8)
 
     -- Process key input
     while player.onGround and stat(30) do
@@ -141,90 +214,46 @@ function _update()
 
     -- Apply gravity
     if player.onGround == false then
-        player.vy += .2
+        player.vy += .3
     else
-        if player.vy > 0 then -- if just landed
+        if player.vy >= 0 then -- if just landed
             player.vx = 0
         end
     end
     
-    new_pos_x = player.x + player.vx
-    new_pos_y = player.y + player.vy
-    --printh("x " .. new_pos_x .. " y " .. new_pos_y .. " | vx " .. player.vx .. " vy " .. player.vy)
+    local player_new_x = player.x + player.vx
+    local player_new_y = player.y + player.vy
 
     -- Check for collisions
-    local checked_position = check_collision(player, new_pos_x, new_pos_y)
+    local checked_position = check_collision(player_new_x, player_new_y, player.x, player.y)
     player.onGround = checked_position.onGround
-    -- Update player position based on movement
+
     player.x = checked_position.x
     player.y = checked_position.y
-
 end
 
-function _draw()
-    cls()
-    camera(camera_x, camera_y)
+function check_collision(new_x, new_y, x,y)
+    local new_x_unit = new_x / 8
+    local new_y_unit = new_y / 8
+    local x_unit = player.x / 8
+    local y_unit = player.y / 8
+    local onGround = false
 
-    for y = 1, chunk_y_size do
-        for x = 1, chunk_x_size do     
-            local tile = get_tile(x,y).tile
-            if tile > 0 then
-                
-                spr(tile, (x - 1) * 8, (y - 1) * 8)
-                --print((y - 1) * 8, (x - 1) * 8, (y - 1) * 8)
-                --print(y, (x - 1) * 8, (y - 1) * 8)
-            end
-        end
+    if get_tile(new_x_unit, y_unit).tile ~= TILE.NONE or get_tile(new_x_unit, y_unit + 0.999).tile ~= TILE.NONE then
+        new_x_unit = flr(new_x_unit) + 1
+    elseif get_tile(new_x_unit + 1, y_unit).tile ~= TILE.NONE or get_tile(new_x_unit + 1, y_unit + 0.999).tile ~= TILE.NONE then
+        new_x_unit = flr(new_x_unit)
     end
 
-    spr(99, new_pos_x, new_pos_y)
-    spr(player.sprite, player.x, player.y)
-
-end
-
-function get_tile(x, y)
-    if x <= 0 or x > chunk_x_size or y <= 0 or y > chunk_y_size then
-        --printh("(" .. x .. "," .. y .. ") tile index is out of bounds")
-        return {tile = -1}
-    else
-        local index = (y - 1) * chunk_x_size + x
-        return levelgen[index]
+    if get_tile(x_unit, new_y_unit).tile ~= TILE.NONE or get_tile(x_unit+0.999, new_y_unit).tile ~= TILE.NONE then
+        new_y_unit = flr(new_y_unit) + 1
+    elseif get_tile(x_unit, new_y_unit + 1).tile ~= TILE.NONE or get_tile(x_unit+0.999, new_y_unit + 1).tile ~= TILE.NONE then
+        new_y_unit = flr(new_y_unit)
+        onGround = true
     end
-end
 
-function get_tile_at_pos(x, y)   
-    return get_tile(flr(x / 8) + 1, flr(y / 8) + 1)
-end
+    new_x = new_x_unit * 8
+    new_y = new_y_unit * 8
 
-
-function check_collision(p, new_x, new_y)
-    local collision_data = {x = new_x, y = new_y, onGround = false}
-    --local dumb = 0
-    -- four corners of the hit box
-    local top_left_point = {x = new_x, y = new_y}
-    local top_right_point = {x = new_x + p.width, y = new_y}
-    local bottom_left_point = {x=new_x, y=new_y+p.height}
-    local bottom_right_point = {x=new_x + p.width, y=new_y+p.height}
-
-    -- check ground tiles
-    local bottom_tile_at_left_point = get_tile_at_pos(bottom_left_point.x, bottom_left_point.y)
-    local bottom_tile_at_right_point = get_tile_at_pos(bottom_right_point.x, bottom_right_point.y)
-
-    if bottom_tile_at_left_point.tile ~= TILE.NONE or bottom_tile_at_right_point.tile ~= TILE.NONE then
-        collision_data.onGround = true
-        --dumb = 1
-
-        -- use the best point or the left one
-        if  bottom_tile_at_left_point.tile == -1 and bottom_tile_at_right_point.tile ~= -1 then
-            collision_data.y = bottom_tile_at_right_point.y*8-8 - p.height
-        elseif (bottom_tile_at_left_point.tile ~= -1 and bottom_tile_at_right_point.tile == -1) or (bottom_tile_at_left_point.tile ~= -1 and bottom_tile_at_right_point.tile ~= -1) then
-            collision_data.y = bottom_tile_at_left_point.y*8-8 - p.height
-        end
-        
-    end
-    
-    --printh("Bottom_left is at " .. bottom_left_point.y .. " so I see " .. bottom_tile_at_left_point.y .. " at " .. bottom_tile_at_left_point.y*8-8 .. "px. onGround = " .. dumb)
-
-    return collision_data
-
+    return {x = new_x, y = new_y, onGround = onGround} -- this is returning nil for some reason
 end
